93afa7ad2c3b3304479d7f50e38c3d77064364d8
阿君啊 <yichunlin09@gmail.com>
2022-07-31 19:29:29 +0800

    update

--- all.js
+++ all.js
@@ -1,3 +1,13 @@
+const logInWrap = document.querySelector('.login');
+const signUpWrap = document.querySelector('.signup');
+const signUp = document.querySelector('#signUp');
+const logIn = document.querySelector('#logIn');
 
-
-
+signUp.addEventListener('click',(e=>{
+    logInWrap.classList.add('d-none');
+    signUpWrap.classList.remove('d-none');
+}))
+logIn.addEventListener('click',(e=>{
+    signUpWrap.classList.add('d-none');
+    logInWrap.classList.remove('d-none');
+}))
