b20d4b62d8a66340e01b2f667fbb549028c618f8
阿君啊 <yichunlin09@gmail.com>
2022-08-01 10:15:53 +0800

    updateAPI

--- all.js
+++ all.js
@@ -1,7 +1,12 @@
+const APIurl = 'https://todoo.5xcamp.us/users';
 const logInWrap = document.querySelector('.login');
 const signUpWrap = document.querySelector('.signup');
 const signUp = document.querySelector('#signUp');
 const logIn = document.querySelector('#logIn');
+const logInForm = document.querySelector('#logInForm');
+const textWarning = document.querySelector('.text-warning');
+
+// console.log(`${APIurl}/sign_in`);
 
 signUp.addEventListener('click',(e=>{
     logInWrap.classList.add('d-none');
@@ -11,3 +16,24 @@ logIn.addEventListener('click',(e=>{
     signUpWrap.classList.add('d-none');
     logInWrap.classList.remove('d-none');
 }))
+
+logInForm[2].addEventListener('click',(e)=>{
+    callLogIn();
+})
+
+function callLogIn(){
+    let user = {};
+    user.email = logInForm[0].value;
+    user.password = logInForm[1].value;
+    if(user.email.trim() === '' || user.password.trim() === ''){
+        textWarning.textContent = '此欄位不可為空';
+        return;
+    }
+    axios.post(`${APIurl}/sign_in`,user)
+        .then((response)=>{
+            console.log(response);
+        })
+        .catch((error)=>{
+            console.log(error);
+        })
+}
